233d88e18e0bbf2f1aeb73e98e5ad2f01a009cae
Aditya Kumar Singh <adityaaa232004@gmail.com>
2025-09-05 19:06:15 +0530

    get started page done

--- app/src/main/java/com/example/iemride/MainActivity.java
+++ app/src/main/java/com/example/iemride/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.iemride;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        videoView = findViewById(R.id.videoView);
+        // Build the video URI from the raw resource
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.iemridevd;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        // Set a listener to loop the video
+        videoView.setOnCompletionListener(mediaPlayer -> {
+            videoView.start(); // Restart the video when it completes
+        });
+
+        videoView.start(); // Start the video
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Resume video playback when the activity comes to the foreground
+        videoView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Pause video playback when the activity is not in the foreground
+        if (videoView.isPlaying()) {
+            videoView.pause();
+        }
     }
 }
